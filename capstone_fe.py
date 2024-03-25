348a534db56ecf6a7fd8e3e66f656c795cfa5604
cosmomojo <sonicismael2@gmail.com>
2024-03-25 18:10:50 +0000

    Added support for historical data

--- capstone_fe.py
+++ capstone_fe.py
@@ -33,31 +33,50 @@ selectedManufacturer = st.sidebar.selectbox('Manufacturer', data['manufacturer']
 trimmed_data = data[data['manufacturer'] == selectedManufacturer]
 selectedModel = st.sidebar.selectbox('Model', trimmed_data['phone_model'].unique())
 trimmed_data = trimmed_data[trimmed_data['phone_model'] == selectedModel]
-selectedColour = st.sidebar.radio('Colour', trimmed_data['phone_colour'].unique())
+selectedColour = st.sidebar.radio('Colour', sorted(trimmed_data['phone_colour'].unique()))
 trimmed_data = trimmed_data[trimmed_data['phone_colour'] == selectedColour]
-selectedCapacity = st.sidebar.radio('Capacity', trimmed_data['capacity'].unique())
+selectedCapacity = st.sidebar.radio('Capacity', sorted(trimmed_data['capacity'].unique()))
 trimmed_data = trimmed_data[trimmed_data['capacity'] == selectedCapacity]
-selectedCarrier = st.sidebar.radio('Carrier', trimmed_data['network'].unique())
+selectedCarrier = st.sidebar.radio('Carrier', np.flip(trimmed_data['network'].unique()))
 trimmed_data = trimmed_data[trimmed_data['network'] == selectedCarrier]
 selectedGrade = st.sidebar.radio('Grade', sorted(trimmed_data['grade'].unique()))
 
 #Main
 selectedPhone = data[(data['manufacturer'] == selectedManufacturer) & (data['phone_model'] == selectedModel) & (data['phone_colour'] == selectedColour) & (data['capacity'] == selectedCapacity) & (data['network'] == selectedCarrier) & (data['grade'] == selectedGrade)]
 selectedPhone = selectedPhone.iloc[0]
+
+#change all values inside of list for price, trade-in_for_voucher, trade-in_for_cash to float
+selectedPhone['price'] = [float(x) for x in selectedPhone['price']]
+selectedPhone['trade-in_for_voucher'] = [float(x) for x in selectedPhone['trade-in_for_voucher']]
+selectedPhone['trade-in_for_cash'] = [float(x) for x in selectedPhone['trade-in_for_cash']]
+
 st.title(''+selectedPhone.manufacturer+' '+selectedPhone.phone_model)
 maincol1, maincol2 = st.columns(2)
 
 maincol1.columns((1, 8, 1))[1].image(selectedPhone.image_url, use_column_width='auto')
 isVoucherBetter = float(selectedPhone['trade-in_for_voucher'][-1]) > float(selectedPhone['trade-in_for_cash'][-1])+20
 
+hasHistoricalData = len(selectedPhone.price) > 1
+
+if hasHistoricalData:
+    priceFluctuation = f'▲ {selectedPhone.price[-1] - selectedPhone.price[-2]}' if selectedPhone.price[-1] > selectedPhone.price[-2] else f'▼ {selectedPhone.price[-2] - selectedPhone.price[-1]}' if selectedPhone.price[-1] < selectedPhone.price[-2] else '- 0.00'
+    voucherFluctuation = f'▲ {selectedPhone["trade-in_for_voucher"][-1] - selectedPhone["trade-in_for_voucher"][-2]}' if selectedPhone["trade-in_for_voucher"][-1] > selectedPhone["trade-in_for_voucher"][-2] else f'▼ {selectedPhone["trade-in_for_voucher"][-2] - selectedPhone["trade-in_for_voucher"][-1]}' if selectedPhone["trade-in_for_voucher"][-1] < selectedPhone["trade-in_for_voucher"][-2] else '- 0.00'
+    cashFluctuation = f'▲ {selectedPhone["trade-in_for_cash"][-1] - selectedPhone["trade-in_for_cash"][-2]}' if selectedPhone["trade-in_for_cash"][-1] > selectedPhone["trade-in_for_cash"][-2] else f'▼ {selectedPhone["trade-in_for_cash"][-2] - selectedPhone["trade-in_for_cash"][-1]}' if selectedPhone["trade-in_for_cash"][-1] < selectedPhone["trade-in_for_cash"][-2] else '- 0.00'
 
 priceStyle = "font-size: 3rem; font-weight: bold; margin: -1.75rem 0rem; padding: 0;"
 priceStyle2 = "font-size: 2.5rem; font-weight: 600; margin: -1.75rem 0rem; padding: 0;"
 priceSubStyle = "font-size: 1rem; font-weight: normal; margin: 0; padding: 0;"
 bestPriceStyle = "color: green;"
+valueUpStyle = "color: green; margin: -1.25rem 0rem; padding: 0;"
+valueDownStyle = "color: red; margin: -1.25rem 0rem; padding: 0;"
+valueNeutralStyle = "color: grey; margin: -1.25rem 0rem; padding: 0;"
+
 maincol2.markdown(f"<p style='{priceStyle}'>£{str(selectedPhone['price'][-1])} <span style='{priceSubStyle}'>Buying Price</p></p>", unsafe_allow_html=True)
+if hasHistoricalData: maincol2.markdown(f"<p style='{(valueUpStyle if priceFluctuation[0] == '▲' else valueDownStyle if priceFluctuation[0] == '▼' else valueNeutralStyle)}'>{priceFluctuation}</p>", unsafe_allow_html=True)
 maincol2.markdown(f"<p style='{priceStyle2 + (bestPriceStyle if isVoucherBetter else '')}'>£{str(selectedPhone['trade-in_for_voucher'][-1])} <span style='{priceSubStyle}'>Trade in for Voucher</p></p>", unsafe_allow_html=True)
+if hasHistoricalData: maincol2.markdown(f"<p style='{(valueUpStyle if voucherFluctuation[0] == '▲' else valueDownStyle if voucherFluctuation[0] == '▼' else valueNeutralStyle)}'>{voucherFluctuation}</p>", unsafe_allow_html=True)
 maincol2.markdown(f"<p style='{priceStyle2 + (bestPriceStyle if not isVoucherBetter else '')}'>£{str(selectedPhone['trade-in_for_cash'][-1])} <span style='{priceSubStyle}'>Trade in for Cash</p></p>", unsafe_allow_html=True)
+if hasHistoricalData: maincol2.markdown(f"<p style='{(valueUpStyle if cashFluctuation[0] == '▲' else valueDownStyle if cashFluctuation[0] == '▼' else valueNeutralStyle)}'>{cashFluctuation}</p>", unsafe_allow_html=True)
 
 
 conditionEmoji = '🇦' if selectedPhone["grade"] == "A" else '🇧' if selectedPhone["grade"] == "B" else '🇨'
@@ -67,4 +86,13 @@ maincol2.subheader(f'🪛 Condition: {conditionEmoji}')
 maincol2.subheader(f'📶 Network: {selectedPhone["network"]}')
 
 st.button('🔃', on_click=invalidate_cache_and_reload)
-st.caption(f'Last updated: {currentTime.floor("S")}')
+st.caption(f'Last updated: {currentTime.floor("s")}')
+
+if hasHistoricalData:
+    price_data = selectedPhone['price']
+    time_data = pd.to_datetime(selectedPhone['time']).date
+    df_new = pd.DataFrame({'price': price_data, 'time': time_data})
+    st.subheader('Price History')
+    st.line_chart(df_new, x='time', y='price')
+
+# st.line_chart(data.loc[data["phoneid"] == selectedPhone["phoneid"]], x="time", y="price")
